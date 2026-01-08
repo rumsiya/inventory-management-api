d334b45499039d49722ee11a0a03f6c0289bc91c
rumsyia <rumsiya@gmail.com>
2026-01-08 14:08:44 +0530

    product image cloudinay setup

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -11,7 +11,13 @@ class Product extends Model
 {
     protected $table ="products";
     protected $fillable = ['product_name','quantity','price','unit_id','category_id','supplier_id','image'];
-
+    protected $casts = [
+        'unit_id'     => 'integer',
+        'category_id' => 'integer',
+        'supplier_id' => 'integer',
+        'quantity'    => 'integer',
+        'price'       => 'float',
+    ];
     public function getUnit(){
         return $this->belongsTo(Unit::class,'unit_id','id');
     }
